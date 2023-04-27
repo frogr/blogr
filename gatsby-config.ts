74fabf21b28fc821c0f3c628501f29dab7e98f76
frogr <austindanielfrench@gmail.com>
2023-04-26 22:16:48 -0700

    animation

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,7 +14,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-catch-links", "gatsby-plugin-react-helmet", "gatsby", "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap",
+  plugins: ["gatsby-plugin-catch-links", "gatsby", "gatsby-plugin-styled-components", "gatsby-plugin-layout", "gatsby-plugin-image", "gatsby-plugin-sitemap",
   {
     resolve: "gatsby-source-contentful",
     options: {
